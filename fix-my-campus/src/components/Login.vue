1f76cfb3139a2aa7ef6764e58ed7b65e5f618ac3
Diane Hu <dh224@duke.edu>
2018-04-18 00:13:40 -0400

    One login component

---
+++ fix-my-campus/src/components/Login.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-container>
+    <v-layout flex align-center justify-center>
+      <v-form>
+          <v-text-field label="Email" v-model="newEmail" required></v-text-field>
+          <v-text-field label="Password" v-model="newPassword" required></v-text-field>
+          <v-card-actions>
+            <v-btn>Login</v-btn>
+            <v-btn>Sign Up</v-btn>
+          </v-card-actions>
+      </v-form>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+
+  export default {
+    name: "login",
+
+    data(){
+      return{
+        newEmail: '',
+        newPassword: ''
+      }
+    }
+  }
+
+</script>
+
+<style scoped>
+
+</style>
