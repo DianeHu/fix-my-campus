62dc6dc9f584b4a5b0957e16e06be97019ecdde1
Jarod Cahoon <jarod.b.cahoon@gmail.com>
2018-04-27 17:22:59 -0400

    Made some minor changes to Contact

--- fix-my-campus/src/components/Contact.vue
+++ fix-my-campus/src/components/Contact.vue
@@ -1,7 +1,46 @@
 <template>
-  <h1>Contacts</h1>
+  <div class = "compPadding">
+    <h1>Duke Facility Contact Information</h1>
+    <hr>
+    <v-container>
+      <v-card v-for="card in contactList">
+        <v-card-text>
+          {{card.Department}}
+        </v-card-text>
+        <v-card-text>
+          {{card.Email}}
+        </v-card-text>
+        <v-card-text>
+          {{card.Phone}}
+        </v-card-text>
+        <v-card-text>
+          {{card.Website}}
+        </v-card-text>
+      </v-card>
+    </v-container>
+  </div>
 </template>
 
 <script>
+  import contacts from '../data/contactinfo'
 
+  export default{
+    data(){
+      return{
+        contactList : contacts
+      }
+    },
+
+  }
 </script>
+
+<style scoped>
+
+  .compPadding{
+    padding: 5px;
+  }
+
+  h1 {
+    text-align: center;
+  }
+</style>
