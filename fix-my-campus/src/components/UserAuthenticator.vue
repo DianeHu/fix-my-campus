4f0b3558b01beec74058225f51b7bb4d64ab3264
Diane Hu <dh224@duke.edu>
2018-04-28 12:46:58 -0400

    User authentication working

--- fix-my-campus/src/components/UserAuthenticator.vue
+++ fix-my-campus/src/components/UserAuthenticator.vue
@@ -1,23 +1,27 @@
 <template>
-  <div>
-    <v-list>
-      <v-text-field
-        name="Duke ID"
-        label = "Enter your DukeID"
-        v-model = tempUserID
-      ></v-text-field>
-      <v-text-field
-        name="Net ID"
-        label = "Enter your NetID"
-        v-model = tempNetID
-      ></v-text-field>
+  <v-card>
+    <v-card-text>
+      <v-list>
+        <v-text-field
+          name="Duke ID"
+          label = "Enter your DukeID"
+          v-model = tempUserID
+        ></v-text-field>
+        <v-text-field
+          name="Net ID"
+          label = "Enter your NetID"
+          v-model = tempNetID
+        ></v-text-field>
+      </v-list>
+    </v-card-text>
+    <v-card-actions>
       <v-flex align-center>
-        <v-btn v-on:click.native="authenticateInformation()">
+        <v-btn dark v-on:click.native="authenticateInformation()">
           Submit
         </v-btn>
       </v-flex>
-    </v-list>
-  </div>
+    </v-card-actions>
+  </v-card>
 </template>
 
 <script>
@@ -42,37 +46,32 @@
 
     methods: {
       authenticateInformation () {
-        var isStudent = this.checkDukeStudent(this.tempNetID, this.tempUserID)
-        console.log(isStudent);
-        if (isStudent) {
-          userRef.push({
-              name: this.user.name,
-              isAdmin: false
-          })
-          this.tempUserID = ''
-          this.tempNetID = ''
-        } else {
-          alert('Incorrect Duke ID or Net ID, please try again');
-        }
+        this.checkDukeStudent(this.tempNetID, this.tempUserID);
       },
 
       checkDukeStudent (netID, userID) {
-        var student = true
+        var student = true;
         var url = 'https://streamer.oit.duke.edu/ldap/people/netid/' + netID + '?access_token=512c70bd8a6cf54fae040bb6f6bb8ccc'
         const proxyURL = "https://cors-anywhere.herokuapp.com/";
         fetch(proxyURL + url)
           .then(response => response.json())
           .then(contents => {
-            console.log(contents)
             if (contents === undefined || contents.length == 0){
-              student = false
+              student =  false;
             } else {
-              student = contents[0].duid === userID && contents[0].netid === netID.toLowerCase()
+              student =  contents[0].duid == userID && contents[0].netid == netID.toLowerCase()
             }
-          })
-          .catch(() => console.log('Access to ' + url + ' is still blocked.'))
-        console.log('student',student)
-        return student
+            if (student) {
+              userRef.push({
+                name: this.user.name,
+                isAdmin: false
+              })
+              this.tempUserID = ''
+              this.tempNetID = ''
+            } else {
+              alert('Incorrect Duke ID or Net ID, please try again');
+            }
+          }).catch(() => console.log('Access to ' + url + ' is still blocked.'))
       }
     }
   }
