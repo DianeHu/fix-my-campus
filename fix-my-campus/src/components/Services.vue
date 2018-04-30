89ca723ba07e72b0960b9172cdef989ee255732a
Diane Hu <dh224@duke.edu>
2018-04-29 20:51:47 -0400

    Added data, added deleting tags

--- fix-my-campus/src/components/Services.vue
+++ fix-my-campus/src/components/Services.vue
@@ -2,6 +2,9 @@
   <div v-if="person">
     <requestadmin :user="person.name"></requestadmin>
     <categoryrequest :user="person.name"></categoryrequest>
+    <v-layout row justify-center>
+      <v-btn dark href="https://fixmycampus-78290.firebaseio.com/.json" target="_blank">Data</v-btn>
+    </v-layout>
   </div>
 </template>
 
